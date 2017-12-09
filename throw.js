2eb1015acf24240c5010e194fc90db94d05639bf
dankato <apearmor@gmail.com>
2017-12-08 19:53:32 -0800

    throw

---
+++ throw.js
@@ -0,0 +1,30 @@
+// Throw
+
+// Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, it must throw an Error according to the following rules:
+
+// If a is 0, throw an Error with message = Zero Error.
+// If a is negative, throw an Error with message = Negative Error.
+
+function isPositive(a) {
+  if (a > 0) {
+    return "YES";
+  }
+  if (a === 0) {
+    return "Zero Error";
+  }
+  if (a < 0) {
+    return "Negative Error";
+  }
+}
+
+function isPositive(a) {
+  try {
+    if (a > 0) throw "YES";
+    else if (a == 0) throw "Zero Error";
+    else if (a < 0) {
+      throw "Negative Error";
+    }
+  } catch (error) {
+    return error;
+  }
+}
