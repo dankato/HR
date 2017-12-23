71497eb62c0f8d6d4a4eff4c08a1ddfb0fbbacde
dankato <apearmor@gmail.com>
2017-12-23 14:10:33 -0800

    opening up 30 days challenge

---
+++ 30Days/helloWorld.js
@@ -0,0 +1,26 @@
+// class
+// - a collection of variables (fields) & functions (methods)
+
+// variables
+// - name that points to (reference) a location in memory where information associated with that name can be stored.
+
+// function
+// - a sequence of packaged instructions that perform a task
+
+// method
+// - in object oriented programming, method is a type of a function that operates on the field of a class
+
+// object
+// - an istance or variable of a class
+
+function processData(inputString) {
+  // This line of code prints the first line of output
+  console.log("Hello, World.");
+
+  // Write the second line of output that prints the contents of 'inputString' here.
+  console.log(inputString);
+}
+
+processData("Welcome to 30 Days of Code!");
+// Hello World
+// Welcome to 30 Days of Code!
